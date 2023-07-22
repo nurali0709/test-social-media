db2f9136a821022febed8b922f5bc92a5114c815
nurali0709 <imusk793@gmail.com>
2023-07-23 01:05:03 +0500

    added coes

--- social_media/main.py
+++ social_media/main.py
@@ -6,6 +6,7 @@ from .post.router import router as post_router
 from .comment.router import router as comment_router
 from .subscription.router import router as subscription_router
 from .tasks.router import router as celery_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -18,3 +19,16 @@ app.include_router(comment_router)
 app.include_router(subscription_router)
 
 app.include_router(celery_router)
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
