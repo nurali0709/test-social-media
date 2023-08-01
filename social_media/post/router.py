b18a4385b762946c3f8ec69528ccbbbadb30a888
nurali0709 <imusk793@gmail.com>
2023-08-01 18:45:46 +0500

    added comments to search section

--- social_media/post/router.py
+++ social_media/post/router.py
@@ -241,6 +241,19 @@ async def search(q: str):
             if post.id not in unique_posts:
                 unique_posts.add(post.id)
 
+                num_comments = await session.execute(select(func.count(Comment.id)).where(Comment.post_id == post.id))
+                num_comments = num_comments.scalar()
+
+                # Count the number of comment responses for the post
+                num_comment_responses = await session.execute(
+                    select(func.count(CommentResponse.id)
+                           ).join(Comment, Comment.id == CommentResponse.comment_id).where(Comment.post_id == post.id)
+                )
+                num_comment_responses = num_comment_responses.scalar()
+
+                # Calculate the total number of comments and comment responses
+                comments = num_comments + num_comment_responses
+
                 author_username = post.author.username if post.author else None
                 author_name = post.author.name if post.author else None
                 author_surname = post.author.surname if post.author else None
@@ -255,6 +268,7 @@ async def search(q: str):
                     "likes": post.likes,
                     "dislikes": post.dislikes,
                     "views": post.views,
+                    "comments": comments,
                     "author_id": post.author_id,
                     "author_username": author_username,
                     "author_name": author_name,
